1426cb9f57c10ad4dda18b51901ff50df1177fde
simonelanza <lanzarottosimone@gmail.com>
2023-10-18 15:08:45 +0200

    primi lavori

---
+++ README.md
@@ -0,0 +1,1 @@
+##LINUX DAY VERONA 
---
+++ container/users.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+i=1
+passwords=("qoP54Am&" "fUSr3@*o" "m&y5QxM7" "fowLB#8d" "Wf3*Qyh6" "&6bXe%5V" "*9Nbt#B!" "m#KVj%J2" "qZG@@RL9" "Xg!ZWQx3")
+
+while [ $i -le 10 ]; do
+    username="utente$i"
+    password="${passwords[$i-1]}"
+    
+    useradd -m $username  # Crea l'utente con la directory home
+    echo "$username:$password" | chpasswd  # Imposta la password
+    chsh -s bash $username 
+    i=$((i+1))
+done
+
